554f8c8ada0011f7265598b3065e8f984580533b
xianzhiwang <xianzhiwang@tencent.com>
2022-05-06 00:02:44 +0800

    Initial Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/tailwindcss',
+    ],
+    typescript: {
+        strict: true,
+        shim: false,
+        typeCheck: true,
+    },
+    ssr: false,
+})
